a6ed8402d1d670ac20dbd373578547249d8436da
Dylan Sprague <dsprague@docuphase.com>
2019-12-31 14:49:17 -0500

    Add more tests for scanning identifiers

--- test/scanner.test.ts
+++ test/scanner.test.ts
@@ -30,6 +30,46 @@ describe("Scanner", () => {
     });
 
     describe("Identifiers", () => {
+      it("Recognizes a generic word as an identifier", () => {
+        // Arrange
+        const input = "someWord";
+
+        // Act
+        const scanResult = scan(input);
+
+        // Assert
+        if (!isRight(scanResult)) {
+          throw new Error("Scan failed, should have succeeded");
+        }
+
+        expect(scanResult.right).toHaveLength(1);
+
+        if (scanResult.right[0].tokenKind !== "identifier") {
+          throw new Error(`${input} not recognized as identifier`);
+        }
+        expect(scanResult.right[0].name).toBe(input);
+      });
+
+      it("Recognizes a word with digits in it as an identifier", () => {
+        // Arrange
+        const input = "var0";
+
+        // Act
+        const scanResult = scan(input);
+
+        // Assert
+        if (!isRight(scanResult)) {
+          throw new Error("Scan failed, should have succeeded");
+        }
+
+        expect(scanResult.right).toHaveLength(1);
+
+        if (scanResult.right[0].tokenKind !== "identifier") {
+          throw new Error(`${input} not recognized as identifier`);
+        }
+        expect(scanResult.right[0].name).toBe(input);
+      });
+
       it('Recognizes a word with "function" in it as an identifier', () => {
         // Arrange
         const input = "subfunction";
