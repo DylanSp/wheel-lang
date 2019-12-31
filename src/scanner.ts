1d7eb2a68820567ed425da61897501a10d05fd22
Dylan Sprague <dsprague@docuphase.com>
2019-12-31 14:23:56 -0500

    Implement function and return keywords in scanner

--- src/scanner.ts
+++ src/scanner.ts
@@ -89,45 +89,79 @@ export const scan: Scan = (input: string) => {
   while (position < input.length) {
     const char = input[position];
 
-    switch (char) {
-      case "(":
-        tokens.push({
-          tokenKind: "leftParen",
-        });
-        break;
-      case ")":
-        tokens.push({
-          tokenKind: "rightParen",
-        });
-        break;
-      case "{":
-        tokens.push({
-          tokenKind: "leftBrace",
-        });
-        break;
-      case "}":
-        tokens.push({
-          tokenKind: "rightBrace",
-        });
-        break;
-      case "=":
-        tokens.push({
-          tokenKind: "singleEquals",
-        });
-        break;
-      case ",":
-        tokens.push({
-          tokenKind: "comma",
-        });
-        break;
-      case ";":
-        tokens.push({
-          tokenKind: "semicolon",
-        });
-        break;
-    }
+    if (/^function/.test(input.substring(position))) {
+      tokens.push({
+        tokenKind: "function",
+      });
+      position += "function".length;
+    } else if (/^return/.test(input.substring(position))) {
+      tokens.push({
+        tokenKind: "return",
+      });
+      position += "return".length;
+    } else {
+      switch (char) {
+        case "(":
+          tokens.push({
+            tokenKind: "leftParen",
+          });
+          position += 1;
+          break;
+        case ")":
+          tokens.push({
+            tokenKind: "rightParen",
+          });
+          position += 1;
+          break;
+        case "{":
+          tokens.push({
+            tokenKind: "leftBrace",
+          });
+          position += 1;
+          break;
+        case "}":
+          tokens.push({
+            tokenKind: "rightBrace",
+          });
+          position += 1;
+          break;
+        case "=":
+          tokens.push({
+            tokenKind: "singleEquals",
+          });
+          position += 1;
+          break;
+        case ",":
+          tokens.push({
+            tokenKind: "comma",
+          });
+          position += 1;
+          break;
+        case ";":
+          tokens.push({
+            tokenKind: "semicolon",
+          });
+          position += 1;
+          break;
+        default:
+          // case for identifiers
+          const matches = input.substring(position).match(/^[a-zA-Z][a-zA-Z0-9]*/);
+          if (matches === null) {
+            throw new Error("Someone had blunder'd"); // TODO more specific error
+          }
+          const name = matches[0];
 
-    position += 1;
+          if (name === undefined) {
+            throw new Error("Someone had blunder'd"); // TODO more specific error
+          }
+
+          tokens.push({
+            tokenKind: "identifier",
+            name,
+          });
+          position += name.length;
+      }
+    }
   }
 
   if (errors.length > 0) {
--- test/scanner.test.ts
+++ test/scanner.test.ts
@@ -4,27 +4,63 @@ import { isRight } from "fp-ts/lib/Either";
 
 describe("Scanner", () => {
   describe("Successful scans", () => {
-    test.each([
-      ["(", "leftParen"],
-      [")", "rightParen"],
-      ["{", "leftBrace"],
-      ["}", "rightBrace"],
-      ["=", "singleEquals"],
-      [",", "comma"],
-      [";", "semicolon"],
-      // ["function", "function"],
-      // ["return", "return"],
-    ])("Recognizes %s", (input, tokenKind) => {
-      // Act
-      const scanResult = scan(input);
+    describe("Simple tokens", () => {
+      test.each([
+        ["(", "leftParen"],
+        [")", "rightParen"],
+        ["{", "leftBrace"],
+        ["}", "rightBrace"],
+        ["=", "singleEquals"],
+        [",", "comma"],
+        [";", "semicolon"],
+        ["function", "function"],
+        ["return", "return"],
+      ])('Recognizes "%s"', (input, tokenKind) => {
+        // Act
+        const scanResult = scan(input);
 
-      // Assert
-      if (!isRight(scanResult)) {
-        throw new Error("Scan failed, should have succeeded");
-      }
+        // Assert
+        if (!isRight(scanResult)) {
+          throw new Error("Scan failed, should have succeeded");
+        }
 
-      expect(scanResult.right).toHaveLength(1);
-      expect(scanResult.right[0].tokenKind).toBe(tokenKind);
+        expect(scanResult.right).toHaveLength(1);
+        expect(scanResult.right[0].tokenKind).toBe(tokenKind);
+      });
+    });
+
+    describe("Identifiers", () => {
+      it('Recognizes a word with "function" in it as an identifier', () => {
+        // Arrange
+        const input = "subfunction";
+
+        // Act
+        const scanResult = scan(input);
+
+        // Assert
+        if (!isRight(scanResult)) {
+          throw new Error("Scan failed, should have succeeded");
+        }
+
+        expect(scanResult.right).toHaveLength(1);
+        expect(scanResult.right[0].tokenKind).toBe("identifier");
+      });
+
+      it('Recognizes a word with "return" in it as an identifier', () => {
+        // Arrange
+        const input = "subreturn";
+
+        // Act
+        const scanResult = scan(input);
+
+        // Assert
+        if (!isRight(scanResult)) {
+          throw new Error("Scan failed, should have succeeded");
+        }
+
+        expect(scanResult.right).toHaveLength(1);
+        expect(scanResult.right[0].tokenKind).toBe("identifier");
+      });
     });
   });
 });
