bc680169113bcbfd5930c9cc8b913d9c24e34068
Dylan Sprague <dsprague@docuphase.com>
2020-01-02 13:25:17 -0500

    Add more tests for expression parsing

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -72,6 +72,328 @@ describe("Parser", () => {
 
         expect(parseResult.right).toEqual(desiredResult);
       });
+
+      it("Parses { x = 1 * 2; }", () => {
+        // Arrange
+        const tokens: Array<Token> = [
+          {
+            tokenKind: "leftBrace",
+          },
+          {
+            tokenKind: "identifier",
+            name: "x",
+          },
+          {
+            tokenKind: "singleEquals",
+          },
+          {
+            tokenKind: "number",
+            value: 1,
+          },
+          {
+            tokenKind: "operation",
+            operation: "multiply",
+          },
+          {
+            tokenKind: "number",
+            value: 2,
+          },
+          {
+            tokenKind: "semicolon",
+          },
+          {
+            tokenKind: "rightBrace",
+          },
+        ];
+
+        // Act
+        const parseResult = parse(tokens);
+
+        // Assert
+        if (!isRight(parseResult)) {
+          console.error(parseResult.left.message);
+          throw new Error("Parse failed, should have succeeded");
+        }
+
+        const desiredResult: Program = [
+          {
+            statementKind: "assignment",
+            variableName: {
+              tokenKind: "identifier",
+              name: "x",
+            },
+            variableValue: {
+              expressionKind: "binOp",
+              operation: "multiply",
+              leftOperand: {
+                expressionKind: "number",
+                value: 1,
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 2,
+              },
+            },
+          },
+        ];
+
+        expect(parseResult.right).toEqual(desiredResult);
+      });
+
+      it("Parses { x = 1 * 2 - 3; } with proper precedence", () => {
+        // Arrange
+        const tokens: Array<Token> = [
+          {
+            tokenKind: "leftBrace",
+          },
+          {
+            tokenKind: "identifier",
+            name: "x",
+          },
+          {
+            tokenKind: "singleEquals",
+          },
+          {
+            tokenKind: "number",
+            value: 1,
+          },
+          {
+            tokenKind: "operation",
+            operation: "multiply",
+          },
+          {
+            tokenKind: "number",
+            value: 2,
+          },
+          {
+            tokenKind: "operation",
+            operation: "subtract",
+          },
+          {
+            tokenKind: "number",
+            value: 3,
+          },
+          {
+            tokenKind: "semicolon",
+          },
+          {
+            tokenKind: "rightBrace",
+          },
+        ];
+
+        // Act
+        const parseResult = parse(tokens);
+
+        // Assert
+        if (!isRight(parseResult)) {
+          console.error(parseResult.left.message);
+          throw new Error("Parse failed, should have succeeded");
+        }
+
+        const desiredResult: Program = [
+          {
+            statementKind: "assignment",
+            variableName: {
+              tokenKind: "identifier",
+              name: "x",
+            },
+            variableValue: {
+              expressionKind: "binOp",
+              operation: "subtract",
+              leftOperand: {
+                expressionKind: "binOp",
+                operation: "multiply",
+                leftOperand: {
+                  expressionKind: "number",
+                  value: 1,
+                },
+                rightOperand: {
+                  expressionKind: "number",
+                  value: 2,
+                },
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 3,
+              },
+            },
+          },
+        ];
+
+        expect(parseResult.right).toEqual(desiredResult);
+      });
+
+      it("Parses { x = 4 + 5 / 6; } with proper precedence", () => {
+        // Arrange
+        const tokens: Array<Token> = [
+          {
+            tokenKind: "leftBrace",
+          },
+          {
+            tokenKind: "identifier",
+            name: "x",
+          },
+          {
+            tokenKind: "singleEquals",
+          },
+          {
+            tokenKind: "number",
+            value: 4,
+          },
+          {
+            tokenKind: "operation",
+            operation: "add",
+          },
+          {
+            tokenKind: "number",
+            value: 5,
+          },
+          {
+            tokenKind: "operation",
+            operation: "divide",
+          },
+          {
+            tokenKind: "number",
+            value: 6,
+          },
+          {
+            tokenKind: "semicolon",
+          },
+          {
+            tokenKind: "rightBrace",
+          },
+        ];
+
+        // Act
+        const parseResult = parse(tokens);
+
+        // Assert
+        if (!isRight(parseResult)) {
+          console.error(parseResult.left.message);
+          throw new Error("Parse failed, should have succeeded");
+        }
+
+        const desiredResult: Program = [
+          {
+            statementKind: "assignment",
+            variableName: {
+              tokenKind: "identifier",
+              name: "x",
+            },
+            variableValue: {
+              expressionKind: "binOp",
+              operation: "add",
+              leftOperand: {
+                expressionKind: "number",
+                value: 4,
+              },
+              rightOperand: {
+                expressionKind: "binOp",
+                operation: "divide",
+                leftOperand: {
+                  expressionKind: "number",
+                  value: 5,
+                },
+                rightOperand: {
+                  expressionKind: "number",
+                  value: 6,
+                },
+              },
+            },
+          },
+        ];
+
+        expect(parseResult.right).toEqual(desiredResult);
+      });
+
+      it("Parses { x = (7 + 8) / 9; } with proper precedence", () => {
+        // Arrange
+        const tokens: Array<Token> = [
+          {
+            tokenKind: "leftBrace",
+          },
+          {
+            tokenKind: "identifier",
+            name: "x",
+          },
+          {
+            tokenKind: "singleEquals",
+          },
+          {
+            tokenKind: "leftParen",
+          },
+          {
+            tokenKind: "number",
+            value: 7,
+          },
+          {
+            tokenKind: "operation",
+            operation: "add",
+          },
+          {
+            tokenKind: "number",
+            value: 8,
+          },
+          {
+            tokenKind: "rightParen",
+          },
+          {
+            tokenKind: "operation",
+            operation: "divide",
+          },
+          {
+            tokenKind: "number",
+            value: 9,
+          },
+          {
+            tokenKind: "semicolon",
+          },
+          {
+            tokenKind: "rightBrace",
+          },
+        ];
+
+        // Act
+        const parseResult = parse(tokens);
+
+        // Assert
+        if (!isRight(parseResult)) {
+          console.error(parseResult.left.message);
+          throw new Error("Parse failed, should have succeeded");
+        }
+
+        const desiredResult: Program = [
+          {
+            statementKind: "assignment",
+            variableName: {
+              tokenKind: "identifier",
+              name: "x",
+            },
+            variableValue: {
+              expressionKind: "binOp",
+              operation: "divide",
+              leftOperand: {
+                expressionKind: "binOp",
+                operation: "add",
+                leftOperand: {
+                  expressionKind: "number",
+                  value: 7,
+                },
+                rightOperand: {
+                  expressionKind: "number",
+                  value: 8,
+                },
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 9,
+              },
+            },
+          },
+        ];
+
+        expect(parseResult.right).toEqual(desiredResult);
+      });
     });
   });
 });
