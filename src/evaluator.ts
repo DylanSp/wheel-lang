8d1aa943a0c04d7bb1335e93ceddf5782c469fe2
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-02-01 22:58:52 -0500

    Implement basic arithmetic evaluation

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -1,4 +1,4 @@
-import { Program } from "./parser";
+import { Program, Expression } from "./parser";
 import { Either, right } from "fp-ts/lib/Either";
 
 /**
@@ -13,6 +13,32 @@ type EvaluateResult = number;
 
 type Evaluate = (program: Program) => Either<RuntimeError, EvaluateResult>;
 
+const evaluateExpr = (expr: Expression): number => {
+  if (expr.expressionKind === "number") {
+    return expr.value;
+  } else if (expr.expressionKind === "binOp") {
+    const lhsValue = evaluateExpr(expr.leftOperand);
+    const rhsValue = evaluateExpr(expr.rightOperand);
+
+    switch (expr.operation) {
+      case "add":
+        return lhsValue + rhsValue;
+      case "subtract":
+        return lhsValue - rhsValue;
+      case "multiply":
+        return lhsValue * rhsValue;
+      case "divide":
+        return lhsValue / rhsValue;
+    }
+  }
+
+  throw new Error("Not implemented");
+};
+
 export const evaluate: Evaluate = (program) => {
-  return right(1);
+  if (program[0].statementKind === "return") {
+    return right(evaluateExpr(program[0].returnedValue));
+  }
+
+  throw new Error("Not implemented");
 };
--- test/evaluator.test.ts
+++ test/evaluator.test.ts
@@ -28,6 +28,270 @@ describe("Evaluator", () => {
 
         expect(evalResult.right).toBe(1);
       });
+
+      it("Evaluates { return 2; }", () => {
+        // Arrange
+        const ast: Program = [
+          {
+            statementKind: "return",
+            returnedValue: {
+              expressionKind: "number",
+              value: 2,
+            },
+          },
+        ];
+
+        // Act
+        const evalResult = evaluate(ast);
+
+        // Assert
+        if (!isRight(evalResult)) {
+          throw new Error("Evaluation failed, should have succeeded");
+        }
+
+        expect(evalResult.right).toBe(2);
+      });
+
+      it("Evaluates { return 1 + 2; }", () => {
+        // Arrange
+        const ast: Program = [
+          {
+            statementKind: "return",
+            returnedValue: {
+              expressionKind: "binOp",
+              operation: "add",
+              leftOperand: {
+                expressionKind: "number",
+                value: 1,
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 2,
+              },
+            },
+          },
+        ];
+
+        // Act
+        const evalResult = evaluate(ast);
+
+        // Assert
+        if (!isRight(evalResult)) {
+          throw new Error("Evaluation failed, should have succeeded");
+        }
+
+        expect(evalResult.right).toBe(3);
+      });
+
+      it("Evaluates { return 3 - 4; }", () => {
+        // Arrange
+        const ast: Program = [
+          {
+            statementKind: "return",
+            returnedValue: {
+              expressionKind: "binOp",
+              operation: "subtract",
+              leftOperand: {
+                expressionKind: "number",
+                value: 3,
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 4,
+              },
+            },
+          },
+        ];
+
+        // Act
+        const evalResult = evaluate(ast);
+
+        // Assert
+        if (!isRight(evalResult)) {
+          throw new Error("Evaluation failed, should have succeeded");
+        }
+
+        expect(evalResult.right).toBe(-1);
+      });
+
+      it("Evaluates { return 5 * 6; }", () => {
+        // Arrange
+        const ast: Program = [
+          {
+            statementKind: "return",
+            returnedValue: {
+              expressionKind: "binOp",
+              operation: "multiply",
+              leftOperand: {
+                expressionKind: "number",
+                value: 5,
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 6,
+              },
+            },
+          },
+        ];
+
+        // Act
+        const evalResult = evaluate(ast);
+
+        // Assert
+        if (!isRight(evalResult)) {
+          throw new Error("Evaluation failed, should have succeeded");
+        }
+
+        expect(evalResult.right).toBe(30);
+      });
+
+      it("Evaluates { return 8 / 2; }", () => {
+        // Arrange
+        const ast: Program = [
+          {
+            statementKind: "return",
+            returnedValue: {
+              expressionKind: "binOp",
+              operation: "divide",
+              leftOperand: {
+                expressionKind: "number",
+                value: 8,
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 2,
+              },
+            },
+          },
+        ];
+
+        // Act
+        const evalResult = evaluate(ast);
+
+        // Assert
+        if (!isRight(evalResult)) {
+          throw new Error("Evaluation failed, should have succeeded");
+        }
+
+        expect(evalResult.right).toBe(4);
+      });
+
+      it("Evaluates { return 1 + 2 + 3; }", () => {
+        // Arrange
+        const ast: Program = [
+          {
+            statementKind: "return",
+            returnedValue: {
+              expressionKind: "binOp",
+              operation: "add",
+              leftOperand: {
+                expressionKind: "binOp",
+                operation: "add",
+                leftOperand: {
+                  expressionKind: "number",
+                  value: 1,
+                },
+                rightOperand: {
+                  expressionKind: "number",
+                  value: 2,
+                },
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 3,
+              },
+            },
+          },
+        ];
+
+        // Act
+        const evalResult = evaluate(ast);
+
+        // Assert
+        if (!isRight(evalResult)) {
+          throw new Error("Evaluation failed, should have succeeded");
+        }
+
+        expect(evalResult.right).toBe(6);
+      });
+
+      it("Evaluates { return 4 + 5 * 6; }", () => {
+        // Arrange
+        const ast: Program = [
+          {
+            statementKind: "return",
+            returnedValue: {
+              expressionKind: "binOp",
+              operation: "add",
+              leftOperand: {
+                expressionKind: "number",
+                value: 4,
+              },
+              rightOperand: {
+                expressionKind: "binOp",
+                operation: "multiply",
+                leftOperand: {
+                  expressionKind: "number",
+                  value: 5,
+                },
+                rightOperand: {
+                  expressionKind: "number",
+                  value: 6,
+                },
+              },
+            },
+          },
+        ];
+
+        // Act
+        const evalResult = evaluate(ast);
+
+        // Assert
+        if (!isRight(evalResult)) {
+          throw new Error("Evaluation failed, should have succeeded");
+        }
+
+        expect(evalResult.right).toBe(34);
+      });
+
+      it("Evaluates { return 7 * 8 - 9; }", () => {
+        // Arrange
+        const ast: Program = [
+          {
+            statementKind: "return",
+            returnedValue: {
+              expressionKind: "binOp",
+              operation: "subtract",
+              leftOperand: {
+                expressionKind: "binOp",
+                operation: "multiply",
+                leftOperand: {
+                  expressionKind: "number",
+                  value: 7,
+                },
+                rightOperand: {
+                  expressionKind: "number",
+                  value: 8,
+                },
+              },
+              rightOperand: {
+                expressionKind: "number",
+                value: 9,
+              },
+            },
+          },
+        ];
+
+        // Act
+        const evalResult = evaluate(ast);
+
+        // Assert
+        if (!isRight(evalResult)) {
+          throw new Error("Evaluation failed, should have succeeded");
+        }
+
+        expect(evalResult.right).toBe(47);
+      });
     });
   });
 });
