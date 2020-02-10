616ae0954bfbcec299e90f1e34ac9af64b333034
Dylan Sprague <dsprague@docuphase.com>
2020-02-10 13:34:24 -0500

    Implement basic function evaluation using eval-apply

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -6,6 +6,9 @@ import { Identifier } from "./types";
  * TYPES
  */
 
+// TODO use
+type Environment = Record<Identifier, Value>;
+
 export interface RuntimeError {
   message: string;
 }
@@ -22,16 +25,31 @@ const makeNumberValue = (value: number): NumberValue => ({
 
 interface ClosureValue {
   valueKind: "closure";
-  name: Identifier;
+  closureName: Identifier;
+  argNames: Array<Identifier>;
   body: Block;
   env: Record<Identifier, Value>;
 }
+
+const makeClosureValue = (
+  closureName: Identifier,
+  argNames: Array<Identifier>,
+  body: Block,
+  env: Record<Identifier, Value>,
+): ClosureValue => ({
+  valueKind: "closure",
+  closureName,
+  argNames,
+  body,
+  env,
+});
 
 type Value = NumberValue | ClosureValue;
 
 type Evaluate = (program: Program) => Either<RuntimeError, Value>;
 
 const evaluateExpr = (env: Record<Identifier, Value>, expr: Expression): Value => {
+  // TODO refactor to switch
   if (expr.expressionKind === "number") {
     return makeNumberValue(expr.value);
   } else if (expr.expressionKind === "binOp") {
@@ -54,25 +72,53 @@ const evaluateExpr = (env: Record<Identifier, Value>, expr: Expression): Value =
     }
   } else if (expr.expressionKind === "variableRef") {
     return env[expr.variableName];
+  } else if (expr.expressionKind === "funcCall") {
+    const func = evaluateExpr(env, expr.callee);
+    const args = expr.args.map((arg) => evaluateExpr(env, arg));
+    return apply(func, args);
   }
 
   throw new Error("Not implemented");
 };
 
-export const evaluate: Evaluate = (program) => {
-  const env: Record<Identifier, Value> = {};
+const apply = (func: Value, args: Array<Value>): Value => {
+  if (func.valueKind !== "closure") {
+    throw new Error("Attempting to apply non-closure");
+  }
 
-  for (const statement of program) {
+  // TODO check that func.argNames.length === args.length
+
+  const envWithArgs: Record<Identifier, Value> = {};
+  for (let i = 0; i < func.argNames.length; i++) {
+    envWithArgs[func.argNames[i]] = args[i];
+  }
+
+  return evaluateBlock(
+    {
+      ...envWithArgs,
+      ...func.env,
+    },
+    func.body,
+  );
+};
+
+const evaluateBlock = (env: Record<Identifier, Value>, block: Block): Value => {
+  for (const statement of block) {
+    // TODO refactor to switch
     if (statement.statementKind === "return") {
-      return right(evaluateExpr(env, statement.returnedValue));
+      return evaluateExpr(env, statement.returnedValue);
     } else if (statement.statementKind === "assignment") {
       env[statement.variableName] = evaluateExpr(env, statement.variableValue);
     } else if (statement.statementKind === "funcDecl") {
-      throw new Error("Function decl not implemented");
+      env[statement.functionName] = makeClosureValue(statement.functionName, statement.args, statement.body, env);
     } else {
       throw new Error("Not implemented!");
     }
   }
 
   throw new Error("No statements");
+};
+
+export const evaluate: Evaluate = (program) => {
+  return right(evaluateBlock({}, program));
 };
