74eeca22200ee0a57a577daba5a3ff0a000f8a57
Dylan Sprague <dsprague@docuphase.com>
2020-02-14 17:03:26 -0500

    Add tests for running full interpretation pipeline

---
+++ test/full_pipeline.test.ts
@@ -0,0 +1,87 @@
+import "jest";
+import { runProgram } from "../src/full_pipeline";
+import { isRight } from "fp-ts/lib/Either";
+
+describe("Full interpretation pipeline", () => {
+  describe("Correct programs", () => {
+    it("Evaluates { return 1; } to 1", () => {
+      // Arrange
+      const programText = "{ return 1; }";
+
+      // Act
+      const runResult = runProgram(programText);
+
+      // Assert
+      if (!isRight(runResult)) {
+        throw new Error("Program failed, should have succeeded");
+      }
+
+      if (runResult.right.valueKind !== "number") {
+        throw new Error("Program did not return number");
+      }
+
+      expect(runResult.right.value).toBe(1);
+    });
+
+    it("Evaluates { x = 2; return x; } to 1", () => {
+      // Arrange
+      const programText = "{ x = 2; return x; }";
+
+      // Act
+      const runResult = runProgram(programText);
+
+      // Assert
+      if (!isRight(runResult)) {
+        throw new Error("Program failed, should have succeeded");
+      }
+
+      if (runResult.right.valueKind !== "number") {
+        throw new Error("Program did not return number");
+      }
+
+      expect(runResult.right.value).toBe(2);
+    });
+
+    it("Evaluates { function f() { return 3; } return f(); } to 3", () => {
+      // Arrange
+      const programText = "{ function f() { return 3; } return f(); } ";
+
+      // Act
+      const runResult = runProgram(programText);
+
+      // Assert
+      if (!isRight(runResult)) {
+        throw new Error("Program failed, should have succeeded");
+      }
+
+      if (runResult.right.valueKind !== "number") {
+        throw new Error("Program did not return number");
+      }
+
+      expect(runResult.right.value).toBe(3);
+    });
+
+    it("Evaluates { function f(x) { return x + 1; } return f(4); } to 5", () => {
+      // Arrange
+      const programText = "{ function f(x) { return x + 1; } return f(4); }";
+
+      // Act
+      const runResult = runProgram(programText);
+
+      // Assert
+      if (!isRight(runResult)) {
+        console.log(runResult.left.pipelineErrorKind);
+        if (runResult.left.pipelineErrorKind === "scan") {
+          runResult.left.scanErrors.forEach((err) => console.log(err.invalidLexeme));
+        }
+        throw new Error("Program failed, should have succeeded");
+      }
+
+      if (runResult.right.valueKind !== "number") {
+        throw new Error("Program did not return number");
+      }
+
+      expect(runResult.right.value).toBe(5);
+    });
+  });
+});
