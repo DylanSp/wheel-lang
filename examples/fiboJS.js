7a89515097c6a6de7b20c12dee19d89111d23c6c
Dylan Sprague <dsprague@docuphase.com>
2020-03-06 16:22:43 -0500

    Add (recursive) Fibonacci example, comparative JS example

---
+++ examples/fiboJS.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+const input = 30;
+console.log(fibonacci(input));
