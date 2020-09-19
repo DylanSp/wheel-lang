84e8e43c523853467a7daa1379e5253e74b5786c
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-09-18 23:22:55 -0400

    Refactor scanner to check against list of tokens instead of duplicating code

--- src/scanner.ts
+++ src/scanner.ts
@@ -189,256 +189,264 @@ export const scan: Scan = (input: string) => {
     const char = input[position];
     const peekAhead = input[position + 1];
 
-    // TODO add general way of recognizing keywords? same logic for "let"/"function"/"return"/"if"/"else"/"while"
-    if (/^let/.test(input.substring(position)) && !/^[a-zA-Z0-9]/.test(input.substring(position + "let".length))) {
-      tokens.push({
-        tokenKind: "let",
-      });
-      position += "let".length;
-    } else if (
-      /^function/.test(input.substring(position)) &&
-      !/^[a-zA-Z0-9]/.test(input.substring(position + "function".length))
-    ) {
-      tokens.push({
-        tokenKind: "function",
-      });
-      position += "function".length;
-    } else if (
-      /^return/.test(input.substring(position)) &&
-      !/^[a-zA-Z0-9]/.test(input.substring(position + "return".length))
-    ) {
-      tokens.push({
-        tokenKind: "return",
-      });
-      position += "return".length;
-    } else if (/^if/.test(input.substring(position)) && !/^[a-zA-Z0-9]/.test(input.substring(position + "if".length))) {
-      tokens.push({
-        tokenKind: "if",
-      });
-      position += "if".length;
-    } else if (
-      /^else/.test(input.substring(position)) &&
-      !/^[a-zA-Z0-9]/.test(input.substring(position + "else".length))
-    ) {
-      tokens.push({
-        tokenKind: "else",
-      });
-      position += "else".length;
-    } else if (
-      /^while/.test(input.substring(position)) &&
-      !/^[a-zA-Z0-9]/.test(input.substring(position + "while".length))
-    ) {
-      tokens.push({
-        tokenKind: "while",
-      });
-      position += "while".length;
-    } else if (
-      /^true/.test(input.substring(position)) &&
-      !/^[a-zA-Z0-9]/.test(input.substring(position + "true".length))
-    ) {
-      tokens.push({
-        tokenKind: "boolean",
-        isTrue: true,
-      });
-      position += "true".length;
-    } else if (
-      /^false/.test(input.substring(position)) &&
-      !/^[a-zA-Z0-9]/.test(input.substring(position + "false".length))
-    ) {
-      tokens.push({
-        tokenKind: "boolean",
-        isTrue: false,
-      });
-      position += "false".length;
-    } else if (
-      /^null/.test(input.substring(position)) &&
-      !/^[a-zA-Z0-9]/.test(input.substring(position + "null".length))
-    ) {
-      tokens.push({
-        tokenKind: "null",
-      });
-      position += "null".length;
-    } else if (/\d{1}/.test(char)) {
-      const numberMatches = input.substring(position).match(/^[\d]+([.][\d]+)?/);
-      if (numberMatches === null) {
-        throw new Error("Programming error when trying to scan a number"); // shouldn't get here; the regex for the whole number should find something if the /\d{1}/ regex matched
-      }
-      const value = parseFloat(numberMatches[0]);
-      tokens.push({
-        tokenKind: "number",
-        value,
-      });
-      position += numberMatches[0].length; // advance past the number we just scanned
-    } else {
-      switch (char) {
-        case "(":
+    switch (char) {
+      case "(":
+        tokens.push({
+          tokenKind: "leftParen",
+        });
+        position += 1;
+        break;
+      case ")":
+        tokens.push({
+          tokenKind: "rightParen",
+        });
+        position += 1;
+        break;
+      case "{":
+        tokens.push({
+          tokenKind: "leftBrace",
+        });
+        position += 1;
+        break;
+      case "}":
+        tokens.push({
+          tokenKind: "rightBrace",
+        });
+        position += 1;
+        break;
+      case "=":
+        if (peekAhead === "=") {
           tokens.push({
-            tokenKind: "leftParen",
+            tokenKind: "doubleEquals",
+          });
+          position += 2;
+        } else {
+          tokens.push({
+            tokenKind: "singleEquals",
           });
           position += 1;
-          break;
-        case ")":
+        }
+        break;
+      case "<":
+        if (peekAhead === "=") {
           tokens.push({
-            tokenKind: "rightParen",
+            tokenKind: "lessThanEquals",
+          });
+          position += 2;
+        } else {
+          tokens.push({
+            tokenKind: "lessThan",
           });
           position += 1;
-          break;
-        case "{":
+        }
+        break;
+      case ">":
+        if (peekAhead === "=") {
           tokens.push({
-            tokenKind: "leftBrace",
+            tokenKind: "greaterThanEquals",
+          });
+          position += 2;
+        } else {
+          tokens.push({
+            tokenKind: "greaterThan",
           });
           position += 1;
-          break;
-        case "}":
+        }
+        break;
+      case ",":
+        tokens.push({
+          tokenKind: "comma",
+        });
+        position += 1;
+        break;
+      case ";":
+        tokens.push({
+          tokenKind: "semicolon",
+        });
+        position += 1;
+        break;
+      case ":":
+        tokens.push({
+          tokenKind: "colon",
+        });
+        position += 1;
+        break;
+      case ".":
+        tokens.push({
+          tokenKind: "period",
+        });
+        position += 1;
+        break;
+      case "+":
+        tokens.push({
+          tokenKind: "plus",
+        });
+        position += 1;
+        break;
+      case "-":
+        tokens.push({
+          tokenKind: "minus",
+        });
+        position += 1;
+        break;
+      case "*":
+        tokens.push({
+          tokenKind: "asterisk",
+        });
+        position += 1;
+        break;
+      case "/":
+        if (peekAhead === "=") {
           tokens.push({
-            tokenKind: "rightBrace",
+            tokenKind: "notEqual",
+          });
+          position += 2;
+        } else {
+          tokens.push({
+            tokenKind: "forwardSlash",
           });
           position += 1;
+        }
+        break;
+      case "&":
+        tokens.push({
+          tokenKind: "ampersand",
+        });
+        position += 1;
+        break;
+      case "|":
+        tokens.push({
+          tokenKind: "verticalBar",
+        });
+        position += 1;
+        break;
+      case "!":
+        tokens.push({
+          tokenKind: "exclamationPoint",
+        });
+        position += 1;
+        break;
+      default:
+        // check for whitespace; if present, skip past it
+        if (/\s/.test(char)) {
+          position += 1;
           break;
-        case "=":
-          if (peekAhead === "=") {
-            tokens.push({
-              tokenKind: "doubleEquals",
-            });
-            position += 2;
-          } else {
-            tokens.push({
-              tokenKind: "singleEquals",
-            });
-            position += 1;
+        }
+
+        if (/\d{1}/.test(char)) {
+          const numberMatches = input.substring(position).match(/^[\d]+([.][\d]+)?/);
+          if (numberMatches === null) {
+            throw new Error("Programming error when trying to scan a number"); // shouldn't get here; the regex for the whole number should find something if the /\d{1}/ regex matched
           }
-          break;
-        case "<":
-          if (peekAhead === "=") {
-            tokens.push({
-              tokenKind: "lessThanEquals",
-            });
-            position += 2;
-          } else {
-            tokens.push({
-              tokenKind: "lessThan",
-            });
-            position += 1;
-          }
-          break;
-        case ">":
-          if (peekAhead === "=") {
-            tokens.push({
-              tokenKind: "greaterThanEquals",
-            });
-            position += 2;
-          } else {
-            tokens.push({
-              tokenKind: "greaterThan",
-            });
-            position += 1;
-          }
-          break;
-        case ",":
+          const value = parseFloat(numberMatches[0]);
           tokens.push({
-            tokenKind: "comma",
+            tokenKind: "number",
+            value,
           });
-          position += 1;
+          position += numberMatches[0].length; // advance past the number we just scanned
           break;
-        case ";":
-          tokens.push({
-            tokenKind: "semicolon",
-          });
-          position += 1;
-          break;
-        case ":":
-          tokens.push({
-            tokenKind: "colon",
-          });
-          position += 1;
-          break;
-        case ".":
-          tokens.push({
-            tokenKind: "period",
-          });
-          position += 1;
-          break;
-        case "+":
-          tokens.push({
-            tokenKind: "plus",
-          });
-          position += 1;
-          break;
-        case "-":
-          tokens.push({
-            tokenKind: "minus",
-          });
-          position += 1;
-          break;
-        case "*":
-          tokens.push({
-            tokenKind: "asterisk",
-          });
-          position += 1;
-          break;
-        case "/":
-          if (peekAhead === "=") {
-            tokens.push({
-              tokenKind: "notEqual",
-            });
-            position += 2;
-          } else {
-            tokens.push({
-              tokenKind: "forwardSlash",
-            });
-            position += 1;
-          }
-          break;
-        case "&":
-          tokens.push({
-            tokenKind: "ampersand",
-          });
-          position += 1;
-          break;
-        case "|":
-          tokens.push({
-            tokenKind: "verticalBar",
-          });
-          position += 1;
-          break;
-        case "!":
-          tokens.push({
-            tokenKind: "exclamationPoint",
-          });
-          position += 1;
-          break;
-        default:
-          // check for whitespace; if present, skip past it
-          if (/\s/.test(char)) {
-            position += 1;
+        }
+
+        const keywords: Array<[string, Token]> = [
+          [
+            "let",
+            {
+              tokenKind: "let",
+            },
+          ],
+          [
+            "function",
+            {
+              tokenKind: "function",
+            },
+          ],
+          [
+            "return",
+            {
+              tokenKind: "return",
+            },
+          ],
+          [
+            "if",
+            {
+              tokenKind: "if",
+            },
+          ],
+          [
+            "else",
+            {
+              tokenKind: "else",
+            },
+          ],
+          [
+            "while",
+            {
+              tokenKind: "while",
+            },
+          ],
+          [
+            "true",
+            {
+              tokenKind: "boolean",
+              isTrue: true,
+            },
+          ],
+          [
+            "false",
+            {
+              tokenKind: "boolean",
+              isTrue: false,
+            },
+          ],
+          [
+            "null",
+            {
+              tokenKind: "null",
+            },
+          ],
+        ];
+
+        let keywordMatched = false;
+        for (const [keyword, token] of keywords) {
+          if (
+            input.substring(position, position + keyword.length) === keyword &&
+            !/^[a-zA-Z0-9]/.test(input.substring(position + keyword.length)) // make sure we don't accidentally match an identifier starting with a keyword
+          ) {
+            tokens.push(token);
+            position += keyword.length;
+            keywordMatched = true;
             break;
           }
+        }
 
-          // check for valid identifier; if doesn't match, lexeme is invalid
-          const matches = input.substring(position).match(/^[a-zA-Z][a-zA-Z0-9]*/);
-          if (matches === null) {
-            const badMatches = input.substring(position).match(/^[^\s]+/);
-            if (badMatches === null) {
-              throw new Error(
-                `Programming error; ${input.substring(
-                  position,
-                )} begins with neither a valid identifier nor an invalid lexeme`,
-              );
-            }
-            errors.push({
-              invalidLexeme: badMatches[0],
-            });
-            position += badMatches[0].length;
-          } else {
-            const name = matches[0];
+        if (keywordMatched) {
+          break;
+        }
 
-            tokens.push({
-              tokenKind: "identifier",
-              name: identifierIso.wrap(name),
-            });
-            position += name.length;
+        // check for valid identifier; if doesn't match, lexeme is invalid
+        const matches = input.substring(position).match(/^[a-zA-Z][a-zA-Z0-9]*/);
+        if (matches === null) {
+          const badMatches = input.substring(position).match(/^[^\s]+/);
+          if (badMatches === null) {
+            throw new Error(
+              `Programming error; ${input.substring(
+                position,
+              )} begins with neither a valid identifier nor an invalid lexeme`,
+            );
           }
-      }
+          errors.push({
+            invalidLexeme: badMatches[0],
+          });
+          position += badMatches[0].length;
+        } else {
+          const name = matches[0];
+
+          tokens.push({
+            tokenKind: "identifier",
+            name: identifierIso.wrap(name),
+          });
+          position += name.length;
+        }
     }
   }
 
