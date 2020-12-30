01d10ecccbbb9c3a707aae134dd11cdcc6d86d4a
Dylan Sprague <dylan.richard.sprague@gmail.com>
2020-12-30 18:55:21 -0500

    Modify main driver to use fs.readFileSync

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { promisify } from "util";
-import { readFile as fsReadFile } from "fs";
+import { readFileSync } from "fs";
 import { isLeft } from "fp-ts/lib/Either";
 import { runProgram } from "./full_pipeline";
 
@@ -10,43 +9,38 @@ if (args.length < 1) {
   process.exit(1);
 }
 
-// TODO use readFileSync - https://nodejs.org/api/fs.html#fs_fs_readfilesync_path_options
-const readFile = promisify(fsReadFile);
-
 try {
-  (async (): Promise<void> => {
-    const programTexts = await Promise.all(args.map(async (filename) => await readFile(filename, "utf8")));
-    const runResult = runProgram(programTexts);
+  const programTexts = args.map((filename) => readFileSync(filename, "utf8"));
+  const runResult = runProgram(programTexts);
 
-    if (isLeft(runResult)) {
-      switch (runResult.left.pipelineErrorKind) {
-        case "scan":
-          console.log("Scan error(s). Invalid lexeme(s):");
-          runResult.left.scanErrors.forEach((scanErr) => console.log(scanErr.invalidLexeme));
-          break;
-        case "parse":
-          console.log("Parse error(s):");
-          runResult.left.parseErrors.forEach((parseErr) => console.log(parseErr.message));
-          break;
-        case "circularDep":
-          console.log("Circular dependencies detected");
-          break;
-        case "evaluation":
-          console.log("Evaluation error:");
-          console.log(runResult.left.evalError);
-          break;
-      }
-    } else {
-      console.log("Successful evaluation.");
-      switch (runResult.right.valueKind) {
-        case "number":
-          console.log(`Result: ${runResult.right.value}`);
-          break;
-        case "closure":
-          console.log("Result: <closure>");
-      }
+  if (isLeft(runResult)) {
+    switch (runResult.left.pipelineErrorKind) {
+      case "scan":
+        console.log("Scan error(s). Invalid lexeme(s):");
+        runResult.left.scanErrors.forEach((scanErr) => console.log(scanErr.invalidLexeme));
+        break;
+      case "parse":
+        console.log("Parse error(s):");
+        runResult.left.parseErrors.forEach((parseErr) => console.log(parseErr.message));
+        break;
+      case "circularDep":
+        console.log("Circular dependencies detected");
+        break;
+      case "evaluation":
+        console.log("Evaluation error:");
+        console.log(runResult.left.evalError);
+        break;
     }
-  })();
+  } else {
+    console.log("Successful evaluation.");
+    switch (runResult.right.valueKind) {
+      case "number":
+        console.log(`Result: ${runResult.right.value}`);
+        break;
+      case "closure":
+        console.log("Result: <closure>");
+    }
+  }
 } catch (err) {
   console.error(`Error reading files`);
   console.error(err);
